fb3159d216d758ff798f8c740c22ad16459a16ce
Eric Douglas <ericalandouglas@gmail.com>
2018-05-09 14:38:18 -0700

    reformated timestamp incrementing where clause and added hard limit to query

--- src/main/java/io/confluent/connect/jdbc/source/TimestampIncrementingTableQuerier.java
+++ src/main/java/io/confluent/connect/jdbc/source/TimestampIncrementingTableQuerier.java
@@ -105,6 +105,7 @@ public class TimestampIncrementingTableQuerier extends TableQuerier {
     } else if (timestampColumn != null) {
       timestampWhereClause(builder, quoteString);
     }
+    builder.append(" LIMIT 10000");
     String queryString = builder.toString();
     log.debug("{} prepared SQL query: {}", this, queryString);
     stmt = db.prepareStatement(queryString);
@@ -126,14 +127,11 @@ public class TimestampIncrementingTableQuerier extends TableQuerier {
     // We should capture both id = 22 (an update) and id = 23 (a new row)
     builder.append(" WHERE ");
     builder.append(JdbcUtils.quoteString(timestampColumn, quoteString));
-    builder.append(" < ? AND ((");
+    builder.append(" < ? AND (");
     builder.append(JdbcUtils.quoteString(timestampColumn, quoteString));
-    builder.append(" = ? AND ");
+    builder.append(", ");
     builder.append(JdbcUtils.quoteString(incrementingColumn, quoteString));
-    builder.append(" > ?");
-    builder.append(") OR ");
-    builder.append(JdbcUtils.quoteString(timestampColumn, quoteString));
-    builder.append(" > ?)");
+    builder.append(") > (?, ?)");
     builder.append(" ORDER BY ");
     builder.append(JdbcUtils.quoteString(timestampColumn, quoteString));
     builder.append(",");
@@ -173,7 +171,6 @@ public class TimestampIncrementingTableQuerier extends TableQuerier {
       stmt.setTimestamp(1, endTime, DateTimeUtils.UTC_CALENDAR.get());
       stmt.setTimestamp(2, tsOffset, DateTimeUtils.UTC_CALENDAR.get());
       stmt.setLong(3, incOffset);
-      stmt.setTimestamp(4, tsOffset, DateTimeUtils.UTC_CALENDAR.get());
       log.debug(
           "Executing prepared statement with start time value = {} end time = {} and incrementing"
           + " value = {}",
